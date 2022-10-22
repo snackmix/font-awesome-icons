c423431cd31f2acf89f7a104b20162d7429328cc
SnackMix <ericg2013@gmail.com>
2022-10-22 02:59:48 -0400

    Update 6.x

--- convert.js
+++ convert.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const axios = require('axios');
 const {get} = require('lodash');
 
-axios.get('https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/metadata/icons.json').then(function (resp) {
+axios.get('https://raw.githubusercontent.com/FortAwesome/Font-Awesome/6.x/metadata/icons.json').then(function (resp) {
     convertData(resp.data);
 });
 
@@ -10,15 +10,17 @@ function convertData(data) {
     const results = new Array;
     for (const name in data) {
         const icon = get(data, name);
-        for (const style of icon.styles) {
-            results.push({
+        const aliases = get(icon, 'aliases.names', new Array);
+        for (const style of icon.free) {
+            const style_data = {
                 name: name,
                 class: getBrandClass(name, style),
                 style: style,
-                search: icon.search.terms,
+                search: icon.search.terms.concat(aliases),
                 unicode: icon.unicode,
                 label: icon.label
-            });
+            };
+            results.push(style_data);
         }
     }
     writeJsonFile('data.json', results);
